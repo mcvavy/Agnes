250a4553bcfe3026d2cde0f29d502cd279dc4968
Michael Oyibo <mcvavy@gmail.com>
2019-09-28 04:29:57 +0100

    Removed dependency on wit.ai

--- modules/Weather.py
+++ modules/Weather.py
@@ -4,27 +4,60 @@ import json
 import pyowm
 owm = pyowm.OWM('dcac096e8c94a58b502991795e61f6d4')  # You MUST provide a valid API key
 
-from wit import Wit
-witClient = Wit("JTUTA7EED6JP2VNJPJJOQKT3P7UPQ2HK")
+# from wit import Wit
+# witClient = Wit("JTUTA7EED6JP2VNJPJJOQKT3P7UPQ2HK")
 
 WORDS = ["WEATHER, FORECAST, TEMPERATURE, HOT, COLD, WARM"]
 
 
+#Using StanfordNer Tagger
+from nltk.tag import StanfordNERTagger
+from nltk.tokenize import word_tokenize
+
+st = StanfordNERTagger('./NER/english.muc.7class.distsim.crf.ser.gz', './NER/stanford-ner.jar', encoding='utf-8')
+
+
 def handle(text, mic):
-    try:
-        response = witClient.message(text)
-    except:
-        mic.say("Something went wrong with the request")
+    tokenized_text = word_tokenize(text.upper())
+    classified_text = st.tag(tokenized_text)
 
-    print(json.dumps(response, indent=2))
+    res = []
+    for pair in classified_text:
+        if pair[1] == "LOCATION":
+            res.append(pair[0])
 
-    try:
+    location = ",".join(res)
 
-        if 'intent' in response['entities']:
-            processIntent(response['entities']['intent'][0]['value'], response, mic)
+    if bool(re.search(r'\btemperature\b', text, re.IGNORECASE)) or \
+            bool(re.search(r'\bcold\b', text, re.IGNORECASE)) or \
+           bool(re.search(r'\bwarm\b', text, re.IGNORECASE)) or \
+           bool(re.search(r'\brain\b', text, re.IGNORECASE)) or \
+           bool(re.search(r'\bhot\b', text, re.IGNORECASE)):
+        w = fetch_Temperature(location)
+        mic.say("The temperature is {0} degree celcius".format(w.get_temperature('celsius')['temp']))
 
-    except KeyError:
-        mic.say("Sorry I cannot process your request, I am still very young, but am learning very quickly Trust me I am trying.")
+    if bool(re.search(r'\bforecast\b', text, re.IGNORECASE)):
+        w = fetch_Temperature(location)
+        weatherObject = w.get_temperature('celsius')
+        mic.say(
+            "Today's forecast are as follows. Wind speed is {0} miles per hour, humidity is {1}, and temperature is {2} degrees with a low of {3} degrees and a high of {4} degrees. Do have a wonderful day".format(
+                w.get_wind()['speed'], w.get_humidity(), weatherObject['temp'], weatherObject['temp_min'],
+                weatherObject['temp_max']))
+
+    # try:
+    #     response = witClient.message(text)
+    # except:
+    #     mic.say("Something went wrong with the request")
+    #
+    # print(json.dumps(response, indent=2))
+    #
+    # try:
+    #
+    #     if 'intent' in response['entities']:
+    #         processIntent(response['entities']['intent'][0]['value'], response, mic)
+    #
+    # except KeyError:
+    #     mic.say("Sorry I cannot process your request, I am still very young, but am learning very quickly Trust me I am trying.")
 
 def isValid(text):
 
@@ -35,26 +68,26 @@ def isValid(text):
            bool(re.search(r'\brain\b', text, re.IGNORECASE)) or \
            bool(re.search(r'\bhot\b', text, re.IGNORECASE))
 
-def processIntent(intent, response, mic):
-    if intent == 'get_temp':
-        if 'resolved' in response['entities']['location'][0]:
-            mic.say('I have found more than one location for {0}, please be more specific like {0} UK or London UK'.format(
-                response['entities']['location'][0]['value']))
-        else:
-            w = fetch_Temperature(re.sub("\s+", ",", response['entities']['location'][0]['value'].strip()))
-            mic.say("The temperature is {0} degree celcius".format(w.get_temperature('celsius')['temp']))
-
-    if intent == 'get_forecast':
-        if 'resolved' in response['entities']['location'][0]:
-            mic.say('I have found more than one location for {0}, please be more specific like {0} UK or London UK'.format(
-                response['entities']['location'][0]['value']))
-        else:
-            w = fetch_Temperature(re.sub("\s+", ",", response['entities']['location'][0]['value'].strip()))
-            weatherObject = w.get_temperature('celsius')
-            mic.say(
-                "Today's forecast are as follows. Wind speed is {0} miles per hour, humidity is {1}, and temperature is {2} degrees with a low of {3} degrees and a high of {4} degrees. Do have a wonderful day".format(
-                    w.get_wind()['speed'], w.get_humidity(), weatherObject['temp'], weatherObject['temp_min'],
-                    weatherObject['temp_max']))
+# def processIntent(intent, response, mic):
+#     if intent == 'get_temp':
+#         if 'resolved' in response['entities']['location'][0]:
+#             mic.say('I have found more than one location for {0}, please be more specific like {0} UK or London UK'.format(
+#                 response['entities']['location'][0]['value']))
+#         else:
+#             w = fetch_Temperature(re.sub("\s+", ",", response['entities']['location'][0]['value'].strip()))
+#             mic.say("The temperature is {0} degree celcius".format(w.get_temperature('celsius')['temp']))
+#
+#     if intent == 'get_forecast':
+#         if 'resolved' in response['entities']['location'][0]:
+#             mic.say('I have found more than one location for {0}, please be more specific like {0} UK or London UK'.format(
+#                 response['entities']['location'][0]['value']))
+#         else:
+#             w = fetch_Temperature(re.sub("\s+", ",", response['entities']['location'][0]['value'].strip()))
+#             weatherObject = w.get_temperature('celsius')
+#             mic.say(
+#                 "Today's forecast are as follows. Wind speed is {0} miles per hour, humidity is {1}, and temperature is {2} degrees with a low of {3} degrees and a high of {4} degrees. Do have a wonderful day".format(
+#                     w.get_wind()['speed'], w.get_humidity(), weatherObject['temp'], weatherObject['temp_min'],
+#                     weatherObject['temp_max']))
 
 def fetch_Temperature(location):
     observation = owm.weather_at_place(location)
